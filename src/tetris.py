0bce9b55ccde4a3bc2f26529769492c4c6915470
Chris Dunworth <chris@dunworth.com>
2017-04-09 09:10:20 -0500

    added Piece class and render_piece() function; removed Block class; added sample pieces to manipulate

--- src/tetris.py
+++ src/tetris.py
@@ -3,6 +3,8 @@ pygame.init()
 
 screen = pygame.display.set_mode([800,1000])
 
+BOARD_WIDTH = 16
+BOARD_HEIGHT = 20
 BLOCK_SIZE = 50
 BLACK = (0,0,0)
 
@@ -11,32 +13,51 @@ DROP_EVENT = pygame.USEREVENT + 1
 stooge_image = pygame.transform.smoothscale( \
     pygame.image.load("/home/ctd/personal/python/img/stooge.png"), (BLOCK_SIZE, BLOCK_SIZE) )
 
-class Block():
-    def __init__(self, image):
+class Piece():
+    def __init__(self, image, x, y, grid1, grid2, grid3, grid4):
         self.image = image
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
+        self.grids = [grid1, grid2, grid3, grid4]
+        self.grid_index = 0
+
+    def grid(self):
+        return self.grids[self.grid_index]
 
     def move_left(self):
         if self.x > 0:
-            self.x -= BLOCK_SIZE
+            self.x -= 1
 
     def move_right(self):
-        if self.x < screen.get_width() - BLOCK_SIZE:
-            self.x += BLOCK_SIZE
+        if self.x < BOARD_WIDTH - len(self.grid()[0]):
+            self.x += 1
 
     def move_down(self):
-        if self.y < screen.get_height() - BLOCK_SIZE:
-            self.y += BLOCK_SIZE
+        if self.y < BOARD_HEIGHT - len(self.grid()):
+            self.y += 1
 
     def rotate(self):
-        self.image = pygame.transform.rotate( self.image, 90 )
+        self.grid_index = (self.grid_index + 1) % 4
 
-    def position(self):
-        return (self.x, self.y)
+def render_piece( piece, surface ):
+    for y in range( len(piece.grid()) ):
+        for x in range( len(piece.grid()[y]) ):
+            if piece.grid()[y][x] == 1:
+                screenx = (piece.x + x) * BLOCK_SIZE
+                screeny = (piece.y + y) * BLOCK_SIZE
+                surface.blit( piece.image, (screenx, screeny) )
 
+# make grids for "reverse Z" piece
+#grid1 = [[0,1,1],[1,1,0]]
+#grid2 = [[1,0],[1,1],[0,1]]
+#piece = Piece( stooge_image, 0, 0, grid1, grid2, grid1, grid2 )
 
-block = Block( stooge_image )
+# make grids for "L" piece
+grid1 = [[1,1,1],[1,0,0]]
+grid2 = [[1,1],[0,1],[0,1]]
+grid3 = [[0,0,1],[1,1,1]]
+grid4 = [[1,0],[1,0],[1,1]]
+piece = Piece( stooge_image, 0, 0, grid1, grid2, grid3, grid4 )
 
 pygame.time.set_timer( DROP_EVENT, 1000 )
 
@@ -47,31 +68,24 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                block.move_left()
+                piece.move_left()
             elif event.key == pygame.K_RIGHT:
-                block.move_right()
+                piece.move_right()
             elif event.key == pygame.K_UP:
-                block.rotate()
+                piece.rotate()
         elif event.type == DROP_EVENT:
-            block.move_down()
-            
+            piece.move_down()
+
     # draw
     screen.fill(BLACK)
-    screen.blit( block.image, block.position() )
+    render_piece( piece, screen )
     pygame.display.update()
 
 pygame.quit()
 
 
-            
-def new_grid(x, y):
-    grid = []                # empty grid (no rows)
-    for i in range(y):
-        row = []             # new row
-        for j in range(x):
-            row.append( 0 )  # add columns to row
-        grid.append( row )   # add row to grid
-    return grid
 
 
-                
+
+
+
